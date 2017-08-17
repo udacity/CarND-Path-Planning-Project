6f8c4858a6b030d8afc03da7f63673b54bfc0f3a
Rohit Subramanian <rohitsub11@gmail.com>
2017-08-16 20:24:32 -0700

    solving for max acc error and collision.

--- src/main.cpp
+++ src/main.cpp
@@ -202,7 +202,7 @@ int main() {
   int lane = 1;
 
   //reference velocity as close to speed limit as possible
-  double ref_vel = 49.5;
+  double ref_vel = 0.0;
 
   h.onMessage([&ref_vel, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -245,6 +245,45 @@ int main() {
 
             int prev_size = previous_path_x.size();
 
+            if(prev_size > 0) {
+              
+              car_s = end_path_s;
+            }
+
+            bool too_close = false;
+
+            //find ref_v to use
+            for(int i=0; i<sensor_fusion.size(); i++) {
+              
+              //car in my lane
+              float d = sensor_fusion[i][6];
+              
+              if(d<(2+4*lane+2) && d>(2+4*lane-2)) {
+                
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx + vy*vy);
+                double check_car_s = sensor_fusion[i][5];
+
+                check_car_s+=((double)prev_size*.02*check_speed);
+
+                if((check_car_s > car_s) && ((check_car_s-car_s) < 30)) {
+
+                  too_close = true;
+                }
+              }
+            }
+
+            if(too_close) {
+
+              ref_vel -= .224; //~=5m/s^2
+
+            } else if(ref_vel < 49.5) {
+
+              ref_vel += 224;
+
+            }
+
             //create a list of sparsly spaced waypoints.
 
             vector<double> pointsx;
@@ -255,6 +294,7 @@ int main() {
             double ref_yaw = deg2rad(car_yaw);
 
             if(prev_size < 2) {
+
               double prev_car_x = car_x - cos(car_yaw);
               double prev_car_y = car_y - sin(car_yaw);
 
@@ -293,6 +333,7 @@ int main() {
             pointsy.push_back(next_wp2[1]);
 
             for(int i=0; i<pointsx.size(); i++) {
+
               double shift_x = pointsx[i]-ref_x;
               double shift_y = pointsy[i]-ref_y;
 
@@ -311,6 +352,7 @@ int main() {
             // TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
             
             for(int i=0; i<previous_path_x.size(); i++) {
+
               next_x_vals.push_back(previous_path_x[i]);
               next_y_vals.push_back(previous_path_y[i]);
             }
@@ -322,6 +364,7 @@ int main() {
             double x_add_on = 0;
 
             for(int i=0; i<=50-previous_path_x.size(); i++) {
+
               double N = (target_dist/(.02*ref_vel/2.24));
               double x_point = x_add_on+(target_x)/N;
               double y_point = s(x_point);
