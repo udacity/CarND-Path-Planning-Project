952f873bf610bd27bf83e14370e29da6b3f084a2
Rujak Ironhammer <sirrujak@gmail.com>
2018-03-16 12:00:42 -0500

    Fixed issue that was stopping it from running.

--- src/main.cpp
+++ src/main.cpp
@@ -228,7 +228,7 @@ int main() {
 
 
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&ref_vel, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &lane, &debug](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
