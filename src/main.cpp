e038ade4df1a423b9b19d9fb7bd09a5b7556ea21
satoshikumano <satoshi.kumano@kii.com>
2018-03-23 03:57:09 +0100

    Initial impl.

--- src/main.cpp
+++ src/main.cpp
@@ -244,6 +244,15 @@ int main() {
 
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+    double dist_inc = 0.3;
+    for (int i=0; i < 50; ++i)
+    {
+      double next_s = car_s + (i+1)*dist_inc;
+      double next_d = 6;
+      vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+      next_x_vals.push_back(xy[0]);
+      next_y_vals.push_back(xy[1]);
+    }
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
