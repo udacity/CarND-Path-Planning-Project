3426df0a78b867440d01ee1393f7475f6b1554fc
Taylor Raack <taylor@raack.info>
2018-08-07 07:29:43 -1000

    Add OpenSSL and zlib development packges to uWebSockets install script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo apt-get install libuv1-dev
+sudo apt-get install libuv1-dev libssl-dev libz-dev
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
 git checkout e94b6e1
