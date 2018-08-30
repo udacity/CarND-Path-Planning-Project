39c6dc42b4ca022f9bc144c4e8c95df3e798b2a7
Changjie <changjieguan@gmail.com>
2018-08-31 00:25:03 +0200

    Smooth path for lane keeping

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -202,8 +203,13 @@ int main()
 		map_waypoints_dy.push_back(d_y);
 	}
 
-	h.onMessage([&map_waypoints_x, &map_waypoints_y, &map_waypoints_s, &map_waypoints_dx, &map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-																																																					 uWS::OpCode opCode) {
+	// start in lane 1;
+	int lane = 1;
+	// reference velocity
+	double ref_vel = 49.5; //mph
+
+	h.onMessage([&map_waypoints_x, &map_waypoints_y, &map_waypoints_s, &map_waypoints_dx, &map_waypoints_dy, &lane, &ref_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+																																																														uWS::OpCode opCode) {
 		// "42" at the start of the message means there's a websocket message event.
 		// The 4 signifies a websocket message
 		// The 2 signifies a websocket event
@@ -242,24 +248,113 @@ int main()
 					// Sensor Fusion Data, a list of all other cars on the same side of the road.
 					auto sensor_fusion = j[1]["sensor_fusion"];
 
-					json msgJson;
+					size_t prev_size = previous_path_x.size();
+
+					// a list of waypoints, evenly spaced at 30m
+					// then interpolate with a spline and fill more points
+					vector<double> ptsx, ptsy;
+
+					// vehicle frame
+					double ref_x = car_x;
+					double ref_y = car_y;
+					double ref_yaw = deg2rad(car_yaw);
+
+					// add 'previous' points to smooth the waypoints
+					if (prev_size < 2)
+					{
+						// add tangents to smooth the path
+						double prev_car_x = car_x - cos(ref_yaw);
+						double prev_car_y = car_y - sin(ref_yaw);
+
+						ptsx.push_back(prev_car_x);
+						ptsx.push_back(car_x);
+
+						ptsy.push_back(prev_car_y);
+						ptsy.push_back(car_y);
+					}
+					else
+					{
+						ref_x = previous_path_x[prev_size - 1];
+						ref_y = previous_path_y[prev_size - 1];
+
+						double ref_x_prev = previous_path_x[prev_size - 2];
+						double ref_y_prev = previous_path_y[prev_size - 2];
+
+						ptsx.push_back(ref_x_prev);
+						ptsx.push_back(ref_x);
+						ptsy.push_back(ref_y_prev);
+						ptsy.push_back(ref_y);
+					}
+
+					// add three further points
+					vector<vector<double>> next_wps(3);
+					next_wps[0] = getXY(car_s + 30, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+					next_wps[1] = getXY(car_s + 60, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+					next_wps[2] = getXY(car_s + 90, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+					for (size_t i = 0; i < next_wps.size(); ++i)
+					{
+						ptsx.push_back(next_wps[i][0]);
+						ptsy.push_back(next_wps[i][1]);
+					}
+
+					// transform world to vehicle frame
+					for (size_t i = 0; i < ptsx.size(); ++i)
+					{
+						double shift_x = ptsx[i] - ref_x;
+						double shift_y = ptsy[i] - ref_y;
+
+						ptsx[i] = shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw);
+						ptsy[i] = shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
+					}
+
+					tk::spline s;
+					s.set_points(ptsx, ptsy);
+
+					
 
 					vector<double> next_x_vals;
 					vector<double> next_y_vals;
 
 					// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-					double dist_inc = 0.3;
-					for (int i = 0; i < 50; ++i)
+					// start with previous left points
+					for (size_t i = 0; i < previous_path_x.size(); ++i)
 					{
-						double next_s = car_s + (i + 1) * dist_inc;
-						double next_d = 6;
+						next_x_vals.push_back(previous_path_x[i]);
+						next_y_vals.push_back(previous_path_y[i]);
+					}
 
-						vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+					// break up spline
+					double target_x = 30.;
+					double target_y = s(target_x);
+					double target_dist = sqrt(target_x * target_x + target_y * target_y);
 
-						next_x_vals.push_back(xy[0]);
-						next_y_vals.push_back(xy[1]);
+					double x_add_on = 0;
+
+					for (int i = 0; i <= 50 - previous_path_x.size(); ++i)
+					{
+						// 2.24 -> mph to m/s
+						double N = target_dist / (0.02 * ref_vel / 2.24);
+						double x_point = x_add_on + target_x / N;
+						double y_point = s(x_point);
+
+						x_add_on = x_point;
+
+						double x_ref = x_point;
+						double y_ref = y_point;
+
+						// vehicle to world
+						x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+						y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+						x_point += ref_x;
+						y_point += ref_y;
+
+						next_x_vals.push_back(x_point);
+						next_y_vals.push_back(y_point);
 					}
 					// END
+					json msgJson;
 					msgJson["next_x"] = next_x_vals;
 					msgJson["next_y"] = next_y_vals;
 
