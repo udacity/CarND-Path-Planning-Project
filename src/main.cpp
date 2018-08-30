3317db63fb0e6a839015610b6ca12e21241d79a8
Changjie <changjieguan@gmail.com>
2018-08-30 22:43:28 +0200

    test lane keeping

--- src/main.cpp
+++ src/main.cpp
@@ -248,14 +248,16 @@ int main()
 					vector<double> next_y_vals;
 
 					// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-					double dist_inc = 0.5;
-					for(int i=0;i<50;++i)
+					double dist_inc = 0.3;
+					for (int i = 0; i < 50; ++i)
 					{
-						//double next_s = car_s + (i+1)*dist_inc;
-						//double next_d = 6;
+						double next_s = car_s + (i + 1) * dist_inc;
+						double next_d = 6;
 
-						next_x_vals.push_back(car_x + (dist_inc*i)*cos(deg2rad(car_yaw)));
-						next_y_vals.push_back(car_y + (dist_inc*i)*sin(deg2rad(car_yaw)));
+						vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+						next_x_vals.push_back(xy[0]);
+						next_y_vals.push_back(xy[1]);
 					}
 					// END
 					msgJson["next_x"] = next_x_vals;
