50fa3c1bedd16d46098371986343c8f21eaaa97f
Changjie <changjieguan@gmail.com>
2018-09-05 14:42:08 +0200

    Show received data

--- src/main.cpp
+++ src/main.cpp
@@ -222,7 +222,7 @@ int main()
 		// "42" at the start of the message means there's a websocket message event.
 		// The 4 signifies a websocket message
 		// The 2 signifies a websocket event
-		//auto sdata = string(data).substr(0, length);
+		auto sdata = string(data).substr(0, length);
 		cout << sdata << endl;
 		if (length && length > 2 && data[0] == '4' && data[1] == '2')
 		{
