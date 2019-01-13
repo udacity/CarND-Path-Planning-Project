e5d28c7df04460f546fb67f051ec4ef26260a76c
yang song <ysong.sc@gmail.com>
2019-01-13 15:44:19 +0000

    refactor 2

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
-#include "spline.h"
 #include "util.h"
 
 using namespace util;
@@ -124,77 +123,14 @@ int main() {
             }else if(ref_vel < 49.5){
               ref_vel += 0.224;
             }
-            vector<double> ptsx, ptsy;
-            double ref_x = car_x, ref_y = car_y, ref_yaw = deg2rad(car_yaw);
 
-            if(prev_size < 2){
-              // use two points that make the path tangent to the cars
-              double prev_car_x = car_x - cos(car_yaw);
-              double prev_car_y = car_y - sin(car_yaw);
-              ptsx.push_back(prev_car_x);
-              ptsx.push_back(car_x);
-              ptsy.push_back(prev_car_y);
-              ptsy.push_back(car_y);
-            }else{
-              // use previous path's end point as the starting reference
-              ref_x = previous_path_x[prev_size-1];
-              ref_y = previous_path_y[prev_size-1];
-              double ref_x_prev = previous_path_x[prev_size-2];
-              double ref_y_prev = previous_path_y[prev_size-2];
-              ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
-              // use two points that make the path tangent to the previous paht's end point
-              ptsx.push_back(ref_x_prev);
-              ptsx.push_back(ref_x);
-              ptsy.push_back(ref_y_prev);
-              ptsy.push_back(ref_y);
-            }
             // in Frenet add evenly 30m spaced points ahead of the starting reference
-            // I remember I implemented a way point path planner in the samilar way of interpolation :D
             vector<double> next_wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
             vector<double> next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
             vector<double> next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
-            ptsx.push_back(next_wp0[0]);
-            ptsx.push_back(next_wp1[0]);
-            ptsx.push_back(next_wp2[0]);
-            ptsy.push_back(next_wp0[1]);
-            ptsy.push_back(next_wp1[1]);
-            ptsy.push_back(next_wp2[1]);
-            for(int i =0; i<ptsx.size(); i++){
-              // shift car reference angel to 0 degrees (coordinates Transformation)
-              double shift_x = ptsx[i] - ref_x;
-              double shift_y = ptsy[i] - ref_y;
-              ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y*sin(0-ref_yaw));
-              ptsy[i] = (shift_x * sin(0-ref_yaw) + shift_y*cos(0-ref_yaw));
-            }
-            // create a spline
-            tk::spline s;
-            s.set_points(ptsx, ptsy);
-            // start with all of the previous points from last time
-            for(int i=0; i<previous_path_x.size(); i++){
-              next_x_vals.push_back(previous_path_x[i]);
-              next_y_vals.push_back(previous_path_y[i]);
-            }
-            // calculate how to break up spline points so that we travel at our desired reference velocity
-            double target_x = 30.0;
-            double target_y = s(target_x);
-            double target_dist = sqrt((target_x)*(target_x) + (target_y)*(target_y));
-            double x_add_on = 0.0;
-            // fill up the rest of path planner after filling it with previous points
-            for(int i=0; i<50-previous_path_x.size(); i++){
-                double N = (target_dist/(0.02*ref_vel/2.24));
-                double x_point = x_add_on+(target_x)/N;
-                double y_point = s(x_point);
-                x_add_on = x_point;
-                double x_ref = x_point;
-                double y_ref = y_point;
-                // rotate back to global coordinates
-                x_point = (x_ref * cos(ref_yaw) - y_ref*sin(ref_yaw));
-                y_point = (x_ref * sin(ref_yaw) + y_ref*cos(ref_yaw));
-                x_point += ref_x;
-                y_point += ref_y;
-                next_x_vals.push_back(x_point);
-                next_y_vals.push_back(y_point);
-            }
+            vector<vector<double>> interpolated_wps{next_wp0, next_wp1, next_wp2};
+            constructPath(car_x, car_y, car_yaw, ref_vel, previous_path_x, previous_path_y, interpolated_wps, next_x_vals, next_y_vals);
+
             msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
