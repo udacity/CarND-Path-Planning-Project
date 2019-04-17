2942623e3379c47375e148595d33a28bff8a9103
siqb <iqbals84@gmail.com>
2019-04-17 03:38:39 -0700

    Straight driving example from class.

--- src/main.cpp
+++ src/main.cpp
@@ -98,6 +98,11 @@ int main() {
            *   sequentially every .02 seconds
            */
 
+          double dist_inc = 0.5;
+          for (int i = 0; i < 50; ++i) {
+            next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
+            next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+          }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
