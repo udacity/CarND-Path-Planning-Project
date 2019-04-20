fb98e90030791bb8b3d9480166a8dd725ffff131
siqb <iqbals84@gmail.com>
2019-04-20 08:00:24 -0700

    Adds initial pseudocode for calculating costs

--- src/main.cpp
+++ src/main.cpp
@@ -214,6 +214,33 @@ int main() {
   h.run();
 }
 
+double calculateCosts() {
+  double totalCost = goal_distance_cost() +
+                     ineffieciency_cost();
+}
+
+double goal_distance_cost(int goal_lane, int intended_lane, int final_lane, 
+                          double distance_to_goal) {
+  // The cost increases with both the distance of intended lane from the goal
+  //   and the distance of the final lane from the goal. The cost of being out 
+  //   of the goal lane also becomes larger as the vehicle approaches the goal.
+  int delta_d = 2.0 * goal_lane - intended_lane - final_lane;
+  double cost = 1 - exp(-(std::abs(delta_d) / distance_to_goal));
+
+  return cost;
+}
+
+double inefficiency_cost(int target_speed, int intended_lane, int final_lane, 
+                         const std::vector<int> &lane_speeds) {
+  // Cost becomes higher for trajectories with intended lane and final lane 
+  //   that have traffic slower than target_speed.
+  double speed_intended = lane_speeds[intended_lane];
+  double speed_final = lane_speeds[final_lane];
+  double cost = (2.0*target_speed - speed_intended - speed_final)/target_speed;
+
+  return cost;
+}
+
 getValidTrajectories(){
   // 2. Consider waypoint to be goal state
   // 3. Calculate T based on how far waypoint is
