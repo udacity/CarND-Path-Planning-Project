7ca17ab13bf965730bab9efa78fb8df80b29fec5
BensonHuang <BensonHuang@itri.org.tw>
2020-04-27 17:51:51 +0800

    A minor try

--- src/main.cpp
+++ src/main.cpp
@@ -102,7 +102,8 @@ int main() {
            //----------------------------//
            double dist_inc = 0.5;
            for (int i = 0; i < 50; ++i) {
-              double next_s = car_s + (i+1) * dist_inc;
+              // double next_s = car_s + (i+1) * dist_inc;
+              double next_s = car_s + i * dist_inc;
               double next_d = 6.0;
               vector<double> xy = getXY(next_s,next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
               next_x_vals.push_back(xy[0]);
