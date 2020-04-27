175a41cff8ee0593ffa10caafde7519d72b7b860
BensonHuang <BensonHuang@itri.org.tw>
2020-04-27 17:48:33 +0800

    First lane following code

--- src/main.cpp
+++ src/main.cpp
@@ -102,8 +102,11 @@ int main() {
            //----------------------------//
            double dist_inc = 0.5;
            for (int i = 0; i < 50; ++i) {
-              next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
-              next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+              double next_s = car_s + (i+1) * dist_inc;
+              double next_d = 6.0;
+              vector<double> xy = getXY(next_s,next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+              next_x_vals.push_back(xy[0]);
+              next_y_vals.push_back(xy[1]);
           }
            //----------------------------//
 
