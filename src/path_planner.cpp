04a1cd3b315562b542c859d09251d56c6b30b356
sajith <sajith@digitalxlabs.com>
2021-02-26 19:03:48 +0530

    Initila tests done

--- src/path_planner.cpp
+++ src/path_planner.cpp
@@ -2,6 +2,7 @@
 // Created by sajith on 2/26/21.
 //
 
+#include <cmath>
 #include "path_planner.h"
 
 using namespace path_planning;
@@ -16,6 +17,13 @@ std::pair<std::vector<double>, std::vector<double >> PathPlanner::planPath(
 
     std::vector<double> next_x_vals;
     std::vector<double> next_y_vals;
+
+    double dist_inc = 0.5;
+    for (int i = 0; i < 50; ++i)
+    {
+        next_x_vals.push_back(simReqData.mainCar.x + (dist_inc * i) * cos(simReqData.mainCar.yaw * 180 / M_PI));
+        next_y_vals.push_back(simReqData.mainCar.y + (dist_inc * i) * sin(simReqData.mainCar.yaw * 180 / M_PI));
+    }
 
     return std::make_pair(next_x_vals, next_y_vals);
 }
