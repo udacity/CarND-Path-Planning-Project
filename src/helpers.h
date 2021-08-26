a694d28c2f52ae580c443096f84dfef7f9a43ce7
Peravene Raveendran <peravene@yahoo.de>
2021-08-26 21:36:14 +0200

    format with google format

--- src/helpers.h
+++ src/helpers.h
@@ -2,6 +2,7 @@
 #define HELPERS_H
 
 #include <math.h>
+
 #include <string>
 #include <vector>
 
@@ -12,17 +13,13 @@ using std::vector;
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 //   else the empty string "" will be returned.
-string hasData(string s)
-{
+string hasData(string s) {
   auto found_null = s.find("null");
   auto b1 = s.find_first_of("[");
   auto b2 = s.find_first_of("}");
-  if (found_null != string::npos)
-  {
+  if (found_null != string::npos) {
     return "";
-  }
-  else if (b1 != string::npos && b2 != string::npos)
-  {
+  } else if (b1 != string::npos && b2 != string::npos) {
     return s.substr(b1, b2 - b1 + 2);
   }
   return "";
@@ -39,25 +36,21 @@ double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
 // Calculate distance between two points
-double distance(double x1, double y1, double x2, double y2)
-{
+double distance(double x1, double y1, double x2, double y2) {
   return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
 
 // Calculate closest waypoint to current x, y position
 int ClosestWaypoint(double x, double y, const vector<double> &maps_x,
-                    const vector<double> &maps_y)
-{
-  double closestLen = 100000; //large number
+                    const vector<double> &maps_y) {
+  double closestLen = 100000;  // large number
   int closestWaypoint = 0;
 
-  for (int i = 0; i < maps_x.size(); ++i)
-  {
+  for (int i = 0; i < maps_x.size(); ++i) {
     double map_x = maps_x[i];
     double map_y = maps_y[i];
     double dist = distance(x, y, map_x, map_y);
-    if (dist < closestLen)
-    {
+    if (dist < closestLen) {
       closestLen = dist;
       closestWaypoint = i;
     }
@@ -68,8 +61,7 @@ int ClosestWaypoint(double x, double y, const vector<double> &maps_x,
 
 // Returns next waypoint of the closest waypoint
 int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
-                 const vector<double> &maps_y)
-{
+                 const vector<double> &maps_y) {
   int closestWaypoint = ClosestWaypoint(x, y, maps_x, maps_y);
 
   double map_x = maps_x[closestWaypoint];
@@ -80,11 +72,9 @@ int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
   double angle = fabs(theta - heading);
   angle = std::min(2 * pi() - angle, angle);
 
-  if (angle > pi() / 2)
-  {
+  if (angle > pi() / 2) {
     ++closestWaypoint;
-    if (closestWaypoint == maps_x.size())
-    {
+    if (closestWaypoint == maps_x.size()) {
       closestWaypoint = 0;
     }
   }
@@ -95,14 +85,12 @@ int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
 // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
 vector<double> getFrenet(double x, double y, double theta,
                          const vector<double> &maps_x,
-                         const vector<double> &maps_y)
-{
+                         const vector<double> &maps_y) {
   int next_wp = NextWaypoint(x, y, theta, maps_x, maps_y);
 
   int prev_wp;
   prev_wp = next_wp - 1;
-  if (next_wp == 0)
-  {
+  if (next_wp == 0) {
     prev_wp = maps_x.size() - 1;
   }
 
@@ -118,21 +106,19 @@ vector<double> getFrenet(double x, double y, double theta,
 
   double frenet_d = distance(x_x, x_y, proj_x, proj_y);
 
-  //see if d value is positive or negative by comparing it to a center point
+  // see if d value is positive or negative by comparing it to a center point
   double center_x = 1000 - maps_x[prev_wp];
   double center_y = 2000 - maps_y[prev_wp];
   double centerToPos = distance(center_x, center_y, x_x, x_y);
   double centerToRef = distance(center_x, center_y, proj_x, proj_y);
 
-  if (centerToPos <= centerToRef)
-  {
+  if (centerToPos <= centerToRef) {
     frenet_d *= -1;
   }
 
   // calculate s value
   double frenet_s = 0;
-  for (int i = 0; i < prev_wp; ++i)
-  {
+  for (int i = 0; i < prev_wp; ++i) {
     frenet_s += distance(maps_x[i], maps_y[i], maps_x[i + 1], maps_y[i + 1]);
   }
 
@@ -144,19 +130,17 @@ vector<double> getFrenet(double x, double y, double theta,
 // Transform from Frenet s,d coordinates to Cartesian x,y
 vector<double> getXY(double s, double d, const vector<double> &maps_s,
                      const vector<double> &maps_x,
-                     const vector<double> &maps_y)
-{
+                     const vector<double> &maps_y) {
   int prev_wp = -1;
 
-  while (s > maps_s[prev_wp + 1] && (prev_wp < (int)(maps_s.size() - 1)))
-  {
+  while (s > maps_s[prev_wp + 1] && (prev_wp < (int)(maps_s.size() - 1))) {
     ++prev_wp;
   }
 
   int wp2 = (prev_wp + 1) % maps_x.size();
 
-  double heading = atan2((maps_y[wp2] - maps_y[prev_wp]),
-                         (maps_x[wp2] - maps_x[prev_wp]));
+  double heading =
+      atan2((maps_y[wp2] - maps_y[prev_wp]), (maps_x[wp2] - maps_x[prev_wp]));
   // the x,y,s along the segment
   double seg_s = (s - maps_s[prev_wp]);
 
@@ -171,4 +155,4 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s,
   return {x, y};
 }
 
-#endif // HELPERS_H
+#endif  // HELPERS_H
